8c0557f19a5970d3309cbc4087e1d7ab3fa91ab5
TeniDevs <148861950+TeniDevs@users.noreply.github.com>
2023-11-13 08:11:40 +0200

    Part 1 Exercises and Practise Notes

---
+++ part1/modulepractice/part1practice/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  return (
+    <div>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
+    </div>
+  )
+}
+
+export default App
